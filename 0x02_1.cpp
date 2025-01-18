d8e6acc66db2e3f7fd6bc473d74751883130ae69
park-jin-a-20 <a47626044@gmail.com>
2025-01-18 15:04:03 +0900

    Add files via upload

---
+++ 0x02_1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+//cin.tie(0);
+//cin.tie(nullptr);
+/*cin명령을 수행하기 전에 cout버퍼를 비우지 않도록 */
+
+int main() {
+	int N, num;
+	cin >> N >> num;
+
+	int* arr = new int[N];
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < N; i++) {
+		if(arr[i]<num)
+		{
+			cout << arr[i] << " ";
+		}
+	}
+}
